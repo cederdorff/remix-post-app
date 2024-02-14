1c3022d6b2ef9329b37de8b2b6f0f8957cdcc3c1
Rasmus Cederdorff <rasmus@cederdorff.com>
2024-02-14 12:35:34 +0100

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,56 +6,80 @@
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-    root: true,
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        ecmaFeatures: {
-            jsx: true
-        }
-    },
-    env: {
-        browser: true,
-        commonjs: true,
-        es6: true
-    },
-    rules: {
-        "react/prop-types": "off"
-    },
-    // Base config
-    extends: ["eslint:recommended"],
+  root: true,
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  globals: {
+    // Because loaders and actions are defined in the app's routes, allow
+    // reading from `process`.
+    process: "readonly"
+  },
 
-    overrides: [
-        {
-            files: ["**/*.{js,jsx}"],
-            plugins: ["react", "jsx-a11y"],
-            extends: [
-                "plugin:react/recommended",
-                "plugin:react/jsx-runtime",
-                "plugin:react-hooks/recommended",
-                "plugin:jsx-a11y/recommended"
-            ],
-            rules: {
-                "react/prop-types": "off"
-            },
-            settings: {
-                react: {
-                    version: "detect"
-                },
-                formComponents: ["Form"],
-                linkComponents: [
-                    { name: "Link", linkAttribute: "to" },
-                    { name: "NavLink", linkAttribute: "to" }
-                ]
-            }
+  // Base config
+  extends: ["eslint:recommended"],
+
+  overrides: [
+    // React
+    {
+      files: ["**/*.{js,jsx,ts,tsx}"],
+      plugins: ["react", "jsx-a11y"],
+      extends: [
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+        "plugin:react-hooks/recommended",
+        "plugin:jsx-a11y/recommended"
+      ],
+      rules: {
+        "react/prop-types": "off" // Disable PropTypes validation
+      },
+      settings: {
+        react: {
+          version: "detect"
         },
-
-        // Node
-        {
-            files: [".eslintrc.js"],
-            env: {
-                node: true
-            }
+        formComponents: ["Form"],
+        linkComponents: [
+          { name: "Link", linkAttribute: "to" },
+          { name: "NavLink", linkAttribute: "to" }
+        ],
+        "import/resolver": {
+          typescript: {}
         }
-    ]
+      }
+    },
+    // Typescript
+    {
+      files: ["**/*.{ts,tsx}"],
+      plugins: ["@typescript-eslint", "import"],
+      parser: "@typescript-eslint/parser",
+      settings: {
+        "import/internal-regex": "^~/",
+        "import/resolver": {
+          node: {
+            extensions: [".ts", ".tsx"]
+          },
+          typescript: {
+            alwaysTryTypes: true
+          }
+        }
+      },
+      extends: ["plugin:@typescript-eslint/recommended", "plugin:import/recommended", "plugin:import/typescript"]
+    },
+    // Node
+    {
+      files: [".eslintrc.js"],
+      env: {
+        node: true
+      }
+    }
+  ]
 };
