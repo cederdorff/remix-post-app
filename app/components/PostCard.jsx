7d802e333ce55643e2bb2f4400d93eb31fbdbf4c
Rasmus Cederdorff <rasmus@cederdorff.com>
2024-02-14 12:05:48 +0100

    styles for <Link>

--- app/components/PostCard.jsx
+++ app/components/PostCard.jsx
@@ -3,7 +3,7 @@ import UserAvatar from "./UserAvatar";
 
 export default function PostCard({ post }) {
   return (
-    <Link to={`/posts/${post.id}`}>
+    <Link className="post-link" to={`/posts/${post.id}`}>
       <article className="post-card">
         <UserAvatar uid={post.uid} />
         <img src={post.image} alt={post.caption} />
