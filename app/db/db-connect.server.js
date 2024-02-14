81d99c75a531b7830aa9f55bbcdcc42a31838fdb
Rasmus Cederdorff <rasmus@cederdorff.com>
2024-02-14 14:20:22 +0100

    Update db-connect.server.js

--- app/db/db-connect.server.js
+++ app/db/db-connect.server.js
@@ -1,16 +1,6 @@
 import { MongoClient, ObjectId } from "mongodb";
 
-const { MONGODB_URL, NODE_ENV } = process.env;
-
-if (!MONGODB_URL) {
-  const errorMessage =
-    NODE_ENV === "production"
-      ? "Please define the MONGODB_URL environment variable — pointing to your full connection string, including database name."
-      : "Please define the MONGODB_URL environment variable inside an .env file — pointing to your full connection string, including database name.";
-  throw new Error(errorMessage);
-}
-
-const client = new MongoClient(MONGODB_URL);
+const client = new MongoClient(process.env.MONGODB_URL);
 
 async function getDatabase() {
   try {
