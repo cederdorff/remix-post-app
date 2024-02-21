987fcabe6eebf26f6ba72422c949bfc9fd1655e7
Rasmus Cederdorff <rasmus@cederdorff.com>
2024-02-21 08:47:53 +0100

    Update posts.$postId.jsx

--- app/routes/posts.$postId.jsx
+++ app/routes/posts.$postId.jsx
@@ -14,19 +14,16 @@ export function meta({ data }) {
 
 export async function loader({ request, params }) {
   // Ensure the user is authenticated
-  await authenticator.isAuthenticated(request, {
+  const authUser = await authenticator.isAuthenticated(request, {
     failureRedirect: "/signin",
   });
   // Load the post and the user who created it
-  const post = await mongoose.models.Post.findById(params.postId).populate(
-    "user"
-  );
-  return json({ post });
+  const post = await mongoose.models.Post.findById(params.postId).populate("user");
+  return json({ post, authUser });
 }
 
 export default function Post() {
-  const { post } = useLoaderData();
-
+  const { post, authUser } = useLoaderData();
   function confirmDelete(event) {
     const response = confirm("Please confirm you want to delete this post.");
     if (!response) {
@@ -38,14 +35,16 @@ export default function Post() {
     <div id="post-page" className="page">
       <h1>{post.caption}</h1>
       <PostCard post={post} />
-      <div className="btns">
-        <Form action="update">
-          <button>Update</button>
-        </Form>
-        <Form action="destroy" method="post" onSubmit={confirmDelete}>
-          <button>Delete</button>
-        </Form>
-      </div>
+      {authUser._id === post.user._id && (
+        <div className="btns">
+          <Form action="update">
+            <button>Update</button>
+          </Form>
+          <Form action="destroy" method="post" onSubmit={confirmDelete}>
+            <button>Delete</button>
+          </Form>
+        </div>
+      )}
     </div>
   );
 }
